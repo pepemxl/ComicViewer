f8d7212861fae68be77a50ac07117e27f910ad48
Jose Alonzo <pepemxl@gmail.com>
2025-11-24 09:12:39 -0600

    Starting backend

---
+++ backend/app/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from .database import Base
+from datetime import datetime
+
+class Comic(Base):
+    __tablename__ = "comics"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, unique=True, index=True)
+    title = Column(String)
+    pages = Column(Integer)
+    cover_path = Column(String)  # ruta temporal o bytes guardados
+    uploaded_at = Column(DateTime, default=datetime.utcnow)
+
+
+class Manga(Base):
+    __tablename__ = "manga"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, unique=True, index=True)
+    title = Column(String)
+    pages = Column(Integer)
+    cover_path = Column(String)  # ruta temporal o bytes guardados
+    uploaded_at = Column(DateTime, default=datetime.utcnow)
+
+
+class Manhwa(Base):
+    __tablename__ = "manhwa"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, unique=True, index=True)
+    title = Column(String)
+    pages = Column(Integer)
+    cover_path = Column(String)  # ruta temporal o bytes guardados
+    uploaded_at = Column(DateTime, default=datetime.utcnow)
